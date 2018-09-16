d9ed867d9dc3266963a0a847f04086747d81bcdb
unknown <max.yeamans@gmail.com>
2018-09-16 11:02:46 -0700

    Updated featured portfolio items.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,35 +2,44 @@ $("document").ready(function () {
     
     // TODO: finish the code below to dynamically create the portfolio photo carousel
     // Portfolio items
-    const portfolioMarvel = {
-        Image: "marvel-teamup.png",
-        Title: "Marvel Teamup",
-        Description: "Pick two Marvel characters and see which comics they first teamed up in. Character not showing? Search for them! This project involved AJAX calls to the Marvel and eBay APIs, storing information in Firebase, and extensive use of jQuery to dynamically generate content.",
-        GitHubName: ""
-    };
-
-    const portfolioTrain = {
-        Image: "train-scheduler.png",
-        Title: "Train Scheduler",
-        Description: "Input information for train lines and dynamically add them to the page. This was an exercise in connecting to a database using Firebase. All time info managed with Moment.js. I added extra code to account for trains stopping at midnight.",
-        GitHubName: "train-scheduler"
-    };
-
-    const portfolioTrivia = {
-        Image: "trivia-game.png",
-        Title: "Star Wars Trivia Game",
-        Description: "Start the game and get 5 random Star Wars questions. This project was meant to work on timers in JavaScript. Each question is stored as its own object in a separate .js file. This project was where jQuery finally clicked for me.",
-        GitHubName: ""
-    };
-
-    const portfolioHangman = {
-        Image: "word-guess.png",
-        Title: "Overwatch Hangman",
-        Description: "This was our first project using vanilla JavaScript. The whole game is written in an object, with event listeners checking for key presses. I added code to make sure you're not penalized for pressing the same wrong letter twice, because I'm nice like that.",
-        GitHubName: "Word-Guess-Game"
-    };
-
-    const portfolioItems = [portfolioMarvel, portfolioTrain, portfolioTrivia, portfolioHangman];
+    
+    const portfolioItems = [
+        {
+            Image: "my-deck.png",
+            Title: "myDeck",
+            Description: "myDeck is the site to keep track of your Magic: The Gathering card collection and create custom decks. This was our second group project for boot camp. I worked mostly on the back end on this one, designing the database models using Sequelize and writing the database API.",
+            RepoLink: "https://github.com/xeroneon/Project-2",
+            PageLink: "https://safe-coast-77807.herokuapp.com/collection"
+        },
+        {
+            Image: "marvel-teamup.png",
+            Title: "Marvel Teamup",
+            Description: "Pick two Marvel characters and see which comics they first teamed up in. Character not showing? Search for them! This project involved AJAX calls to the Marvel and eBay APIs, storing information in Firebase, and extensive use of jQuery to dynamically generate content.",
+            RepoLink: "https://github.com/maxyeamans/soaring-indigo-mimes",
+            PageLink: "https://maxyeamans.github.io/soaring-indigo-mimes"
+        },
+        {
+            Image: "train-scheduler.png",
+            Title: "Train Scheduler",
+            Description: "Input information for train lines and dynamically add them to the page. This was an exercise in connecting to a database using Firebase. All time info managed with Moment.js. I added extra code to account for trains stopping at midnight.",
+            RepoLink: "https://github.com/maxyeamans/train-scheduler",
+            PageLink: "https://maxyeamans.github.io/train-scheduler"
+        },
+        {
+            Image: "trivia-game.png",
+            Title: "Star Wars Trivia Game",
+            Description: "Start the game and get 5 random Star Wars questions. This project was meant to work on timers in JavaScript. Each question is stored as its own object in a separate .js file. This project was where jQuery finally clicked for me.",
+            RepoLink: "https://github.com/maxyeamans/TriviaGame",
+            PageLink: "https://maxyeamans.github.io/TriviaGame"
+        },
+        {
+            Image: "word-guess.png",
+            Title: "Overwatch Hangman",
+            Description: "This was our first project using vanilla JavaScript. The whole game is written in an object, with event listeners checking for key presses. I added code to make sure you're not penalized for pressing the same wrong letter twice, because I'm nice like that.",
+            RepoLink: "https://github.com/maxyeamans/Word-Guess-Game",
+            PageLink: "https://maxyeamans.github.io/Word-Guess-Game"
+        }
+    ];
 
     // Add the portfolio items to the DOM
     portfolioItems.forEach(function (item, index) {
@@ -49,11 +58,11 @@ $("document").ready(function () {
         let portDescription = $("<p>").text(item.Description).addClass("d-none d-md-block mb-0");
         // I could probably do these next two in a loop.
         let portPageLink = $("<a>").text("View Page").attr({
-            href : "https://maxyeamans.github.io/" + item.GitHubName,
+            href : item.PageLink,
             target : "_blank"
         });
         let portSourceLink = $("<a>").text("View Source").attr({
-            href : "https://www.github.com/maxyeamans/" + item.GitHubName,
+            href : item.RepoLink,
             target : "_blank"
         });
         // Add all of the portfolio item info to the caption box
@@ -64,4 +73,4 @@ $("document").ready(function () {
         $("#portfolio-items").append(portItem);
     });
 
-});
+}); 
